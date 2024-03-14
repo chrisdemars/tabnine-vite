4f75b9de31edf621bcc981c2a85fd8bb027cf0fe
Chris DeMars <cdemars@digitalocean.com>
2024-03-14 10:29:10 -0400

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="copyright">
+        &copy; 2023 Tabnine
+      </div>
+      <div className="author">
+        Created by Chris DeMars
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href='#'>Link 1</a>
+          </li>
+          <li>
+            <a href='#'>Link 2</a>
+          </li>
+          <li>
+            <a href='#'>Link 3</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
